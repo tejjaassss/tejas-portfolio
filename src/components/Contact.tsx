9744fa2bc266f1369d9e6e7aa132e2c07935edb5
Tejas Natekar <tejasnatekar3104@gmail.com>
2026-05-24 14:15:25 +0530

    fix: correct GitHub profile link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -26,7 +26,7 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com/tejasnatekar"
+              href="https://github.com/tejjaassss"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
